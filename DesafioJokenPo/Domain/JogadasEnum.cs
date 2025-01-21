6cfccf3308e6e7be1a0e7ca8a1174eac0b970273
Douetts <r_santos07@outlook.com>
2025-01-21 01:20:34 -0300

    projeto

---
+++ DesafioJokenPo/Domain/JogadasEnum.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioJokenPo.Domain
+{
+    public class Jogo
+    {
+        public Jogo(List<Jogador> jogadores)
+        {
+            Jogadores = jogadores;
+        }
+
+        public List<Jogador> Jogadores { get; set; }
+
+        public List<Jogador> Empatou()
+        {
+            var resultado = ResultadoEnum.Èmpate;
+
+            foreach (var item in Jogadores) { 
+                item.Resultado = resultado;
+            }
+
+            return Jogadores;
+        }
+
+        public List<Jogador> Jogar()
+        {
+            IDictionary<JogadasEnum, List<JogadasEnum>> combinacoes = new Dictionary<JogadasEnum, List<JogadasEnum>>()
+        {
+            { JogadasEnum.Pedra, new List<JogadasEnum> { JogadasEnum.Tesoura } },
+            { JogadasEnum.Papel, new List<JogadasEnum> { JogadasEnum.Pedra } },
+            { JogadasEnum.Tesoura, new List<JogadasEnum> { JogadasEnum.Papel } }
+        };
+
+            var jogadasDestintas = Jogadores.Select(x => x.Jogada).Distinct().Count();
+
+            if (jogadasDestintas > 2)
+                return Empatou();
+
+            foreach (var jogada in Jogadores)
+            {
+                var jogadasFortes = combinacoes.Where(x => x.Key == jogada.Jogada).FirstOrDefault();
+
+                if (jogadasFortes.Value == null)
+                    throw new Exception("Jogada não encontrada!");
+
+                var excetoMinhaJogada = Jogadores.Where(x => x.Nome != jogada.Nome).Select(x => x.Jogada).ToList();
+
+                if (jogadasFortes.Value.Where(x => !excetoMinhaJogada.Contains(x)).Any())
+                {
+                    jogada.Resultado = ResultadoEnum.Perdeu;
+                }
+                else
+                {
+                    jogada.Resultado = ResultadoEnum.Ganhou;
+                }
+
+            }
+
+            return Jogadores;
+        }
+    }
+    public class Jogador
+    {
+        public Jogador(string nome, JogadasEnum jogada)
+        {
+            Nome = nome;
+            Jogada = jogada;
+        }
+
+        public string Nome { get; set; }
+        public JogadasEnum Jogada { get; set; }
+        public ResultadoEnum Resultado { get; set; }
+    }
+
+    public enum JogadasEnum
+    {
+        Pedra = 0,
+        Papel = 1,
+        Tesoura = 2,
+    }
+
+    public enum ResultadoEnum
+    {
+        Èmpate = 0,
+        Ganhou = 1,
+        Perdeu = 2,
+    }
+}
+
---
+++ DesafioJokenPo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesafioJokenPo.Domain;
+
+Jogador jogador = new Jogador("Rafael", JogadasEnum.Tesoura);
+Jogador jogador2 = new Jogador("Alanis", JogadasEnum.Pedra);
+Jogador jogador3 = new Jogador("Milene", JogadasEnum.Tesoura);
+
+var jogadores = new List<Jogador>();
+
+jogadores.Add(jogador);
+jogadores.Add(jogador2);
+jogadores.Add(jogador3);
+
+Jogo jogo = new Jogo(jogadores);
+
+var resultado = jogo.Jogar();
+
+foreach (var result in resultado)
+{
+    Console.WriteLine($"o jogador {result.Nome} teve o resultado: {result.Resultado.ToString()}");
+}
+
+Console.ReadLine();
